13a2656ad49f6d5d91ab3b7dbbd63d4836a95149
Piotr Zadrożny <piotr.zadrozny@webplus.net.pl>
2015-04-16 12:06:11 +0200

    Main menu

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// Main menu.
+/// Attached to Main Camera
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	public float startButtonXPosition;
+	public float startButtonYPosition;
+
+	public float startButtonWidth;
+	public float startButtonHeight;
+
+	public int buttonFontSize;
+	public string buttonFont;
+
+
+	void OnGUI(){
+
+		// Display our Background Texture
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+
+		// Display our Buttons
+
+		GUIStyle menuButtonStyle = new GUIStyle(GUI.skin.button);
+
+		menuButtonStyle.fontSize = buttonFontSize;
+		menuButtonStyle.font = (Font)Resources.Load(buttonFont, typeof(Font));
+		menuButtonStyle.normal.textColor = Color.white;
+
+		if (GUI.Button (new Rect(Screen.width * startButtonXPosition, Screen.height * startButtonYPosition, Screen.width * startButtonWidth, Screen.height * startButtonHeight), "Play", menuButtonStyle)) {
+			print ("Clicked Play button");
+			// Application.LoadLevel ('level1');
+		}
+	}
+}
