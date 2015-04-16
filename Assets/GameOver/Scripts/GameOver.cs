124b604716eb721683adae35fef24fb3a1315413
Piotr Zadrożny <piotr.zadrozny@webplus.net.pl>
2015-04-16 15:31:31 +0200

    GameOver scene

---
+++ Assets/GameOver/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// Game over.
+/// Attached to Main Camera
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	void OnGUI(){
+
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+
+	}
+}
---
+++ Assets/TimeMode/Scripts/Finish.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Application.LoadLevel ("gameover");
+	}
+}
