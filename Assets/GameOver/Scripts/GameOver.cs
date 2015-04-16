78eeb15094736ec7cf6e53b19a43a847c6736592
Piotr Zadrożny <piotr.zadrozny@webplus.net.pl>
2015-04-16 18:09:48 +0200

    Added timer

--- Assets/GameOver/Scripts/GameOver.cs
+++ Assets/GameOver/Scripts/GameOver.cs
@@ -3,15 +3,39 @@
 /// Attached to Main Camera
 /// </summary>
 using UnityEngine;
+using System;
 using System.Collections;
 
 public class GameOver : MonoBehaviour {
 
 	public Texture backgroundTexture;
 
+	public float gameOverLabelXPosition;
+	public float gameOverLabelYPosition;
+	public float gameOverLabelWidth;
+	public float gameOverLabelHeight;
+
+	public float timeLabelXPosition;
+	public float timeLabelYPosition;
+	public float timeLabelWidth;
+	public float timeLabelHeight;
+
+	public int fontSize;
+	public string font;
+
 	void OnGUI(){
 
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+		
+		GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
 
+		labelStyle.fontSize = fontSize;
+		labelStyle.font = (Font)Resources.Load(font, typeof(Font));
+		labelStyle.normal.textColor = Color.white;
+
+		int time = PlayerPrefs.GetInt ("time");
+
+		GUI.Label (new Rect (Screen.width * gameOverLabelXPosition, Screen.height * gameOverLabelYPosition, Screen.width * gameOverLabelWidth, Screen.height * gameOverLabelHeight), "Game Over!", labelStyle);
+		GUI.Label (new Rect (Screen.width * timeLabelXPosition, Screen.height * timeLabelYPosition, Screen.width * timeLabelWidth, Screen.height * timeLabelHeight), String.Concat("Time: ", time, "s"), labelStyle);
 	}
 }
---
+++ Assets/TimeMode/Scripts/TimeMode.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class TimeMode : MonoBehaviour {
+
+	public float timeLabelXPosition;
+	public float timeLabelYPosition;
+	public float timeLabelWidth;
+	public float timeLabelHeight;
+
+	public int fontSize;
+	public string font;
+
+	private float time;
+	private string timerLabel;
+
+	void OnGUI(){
+
+		GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+		
+		labelStyle.fontSize = fontSize;
+		labelStyle.font = (Font)Resources.Load(font, typeof(Font));
+		labelStyle.normal.textColor = Color.white;
+		
+		GUI.Label (new Rect (Screen.width * timeLabelXPosition, Screen.height * timeLabelYPosition, Screen.width * timeLabelWidth, Screen.height * timeLabelHeight), timerLabel, labelStyle);
+	}
+
+	// Use this for initialization
+	void Start () {
+		time = 0.0F;
+		timerLabel = "Time: 0s";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		PlayerPrefs.SetInt("time", (int) time);
+		timerLabel = String.Concat("Time: ", (int) time, "s");
+	}
+}
